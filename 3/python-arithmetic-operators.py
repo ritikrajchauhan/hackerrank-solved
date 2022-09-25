0ca0778d9710f01b8987892b94f091a7071dc731
Ritik Raj Chauhan <ritikraj250500@gmail.com>
2022-09-25 16:32:19 +0530

    shifting to other folder

--- 3/python-arithmetic-operators.py
+++
@@ -1,5 +0,0 @@
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    if (a and b) <= pow(10,10) and (a and b) >= 1:
-        print(str(a+b) + '\n' + str(a-b) + '\n' + str(a*b))
--- 7/python-print.py
+++
@@ -1,5 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    if n>=1 and n<=150:
-        for i in range(1,n+1):
-            print(str(i),end='')
